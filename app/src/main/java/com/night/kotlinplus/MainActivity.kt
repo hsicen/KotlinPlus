0e3483c3cc9c682701e52ff57b80fdc67f8fb723
night <codinghuang@163.com>
2019-01-01 22:48:37 +0800

    首次提交

---
+++ app/src/main/java/com/night/kotlinplus/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.night.kotlinplus
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * <p>作者：Night  2019/1/1 22:39
+ * <p>邮箱：codinghuang@163.com
+ * <p>作用：
+ * <p>描述：Kotlin学习笔记
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
