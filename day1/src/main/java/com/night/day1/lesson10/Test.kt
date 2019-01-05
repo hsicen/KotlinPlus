cc93f1137e4917d1e3fb1263b2ddb8f342f58fa9
night <codinghuang@163.com>
2019-01-05 18:07:47 +0800

    访问修饰符和伴生对象

--- day1/src/main/java/com/night/day1/lesson10/Test.kt
+++ day1/src/main/java/com/night/day1/lesson10/Test.kt
@@ -7,5 +7,31 @@ package com.night.day1.lesson10
  * <p>描述：KotlinPlus
  *
  * 访问修饰符：public  protected  private  internal
- *
+ * 伴生对象：companion object
  */
+
+class StringUtils {
+    companion object {
+
+        fun isEmpty(str: String): Boolean {
+            return "" == str
+        }
+    }
+}
+
+class Single private constructor() {
+    companion object {
+
+        fun get(): Single = Holder.instance
+    }
+
+    private object Holder {
+        val instance = Single()
+    }
+}
+
+
+fun main(args: Array<String>) {
+
+    val mInstance = Single.get()
+}
